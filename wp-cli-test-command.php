10ab51501f73969fa9db63511066363d6791f132
Paolo T. <plugins@swergroup.com>
2013-07-03 03:40:22 +0200

    Non-necessary code cleaning

--- wp-cli-test-command.php
+++ wp-cli-test-command.php
@@ -18,7 +18,7 @@ class Unit_Test_Cmd extends WP_CLI_Command{
     public function setup( $args, $assoc_args = array() ){
       list( $target ) = $args;
 
-      # default download URL
+# define default download URL
       $dataurl = 'https://wpcom-themes.svn.automattic.com/demo/theme-unit-test-data.xml';
       $download_url = isset( $assoc_args['data'] ) ? $assoc_args['data'] : $dataurl;
 
@@ -28,11 +28,8 @@ class Unit_Test_Cmd extends WP_CLI_Command{
 
           # WordPress reset/reinstall
           if ( isset( $assoc_args['reset'] ) ):
-            $actual_URL = get_option( 'siteurl' );
-
             # reset wp
             WP_CLI::launch( 'wp db reset --yes' );
-
             # install wp
             WP_CLI::launch(
                 'wp core install '
@@ -48,28 +45,25 @@ class Unit_Test_Cmd extends WP_CLI_Command{
           $silent = WP_CLI::get_config( 'quiet' ) ? '--silent ' : '';
           $cmd = "curl -f $silent $download_url -o /tmp/theme-unit-test-data.xml";
           WP_CLI::launch( $cmd );
-
           # test data xml import
           WP_CLI::launch( 'wp import /tmp/theme-unit-test-data.xml --authors=skip' );
 
-
           # plugins check, install, activation
           $plugins = array(
-            'developer', 
-            'theme-check', 
-            'debug-bar', 
-            'log-deprecated-notices', 
             'debogger', 
+            'debug-bar', 
+            'developer', 
+            'log-deprecated-notices', 
             'monster-widget', 
-            'wordpress-beta-tester', 
             'regenerate-thumbnails',
-            'zendesk',
+            'theme-check', 
+            'wordpress-beta-tester', 
           );
 
           foreach ( $plugins as $plugin ):
             $res = WP_CLI::launch( 'wp plugin status '.$plugin, false );
             if ( isset( $res) && $res === 1 ){
-              # install and activate plugin
+              # download, install and activate plugin
               WP_CLI::launch( 'wp plugin install '.$plugin.' --activate' );
             } else {
               # activate plugin
@@ -78,18 +72,16 @@ class Unit_Test_Cmd extends WP_CLI_Command{
           endforeach;
 
           # option update
-          WP_CLI::launch( 'wp option update blogname "WordPress Theme Unit Test Site"' );
+          WP_CLI::launch( 'wp option update blogname "WordPress Themes Test Site"' );
           WP_CLI::launch( 'wp option update posts_per_page 5' );
           WP_CLI::launch( 'wp option update thread_comments 1' );
           WP_CLI::launch( 'wp option update thread_comments_depth 3' );
           WP_CLI::launch( 'wp option update page_comments 1' );
           WP_CLI::launch( 'wp option update comments_per_page 5' );
-
           WP_CLI::launch( 'wp option update medium_max_w ""' );
           WP_CLI::launch( 'wp option update medium_max_h ""' );
           WP_CLI::launch( 'wp option update large_max_w ""' );
           WP_CLI::launch( 'wp option update large_max_h ""' );
-
           WP_CLI::launch( 'wp option update permalink_structure "/%year%/%monthnum%/%day%/%postname%/"' );
 
           # todo: create long custom menu, all pages
