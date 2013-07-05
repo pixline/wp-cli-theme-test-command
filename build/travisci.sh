1b6352cf1a77e9f232480bb64fcd457b323393f0
pixline <pixline@gmail.com>
2013-07-06 01:36:59 +0200

    travis setup (WIP)

--- build/travisci.sh
+++ build/travisci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wp core download --version=$WP_VERSION --path=$HOME/wordpress
+wp core download --version=$WP_VERSION --path=/tmp/wordpress
 wp core install --url=http://localhost --title="WP" --admin_name=test --admin_email=test@example.org --admin_password=test
 wp theme-test install --menus
---
+++ tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+// phpunit bootstrap
