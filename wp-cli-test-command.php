d568a54e4574aeb40adf424be5f73ce5574b1c71
pixline <pixline@gmail.com>
2013-07-05 21:20:26 +0200

    synopsis

--- wp-cli-test-command.php
+++ wp-cli-test-command.php
@@ -178,7 +178,7 @@ class Theme_Test_Cmd extends WP_CLI_Command{
 	* --menus 								Create custom nav menus (full page list, short random page list)
 	* 
 	* @when after_wp_load
-	* @synopsis [<slug>] [--data=<data>] [--url=<url>] [--title=<title>] [--admin_name=<admin_name>] [--admin_email=<admin_email>] [--admin_password=<admin_password>] [--menus] [--reset] [--dbname] [--dbuser] [--dbpass]
+	* @synopsis [--data=<data>] [--url=<url>] [--title=<title>] [--admin_name=<admin_name>] [--admin_email=<admin_email>] [--admin_password=<admin_password>] [--menus] [--reset] [--dbname] [--dbuser] [--dbpass]
 	*/
 	public function install( $args = null, $assoc_args = array() ){
 
